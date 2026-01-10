f77aa90ba984568ac6b7681bf388545b04f9c40f
wyspr <a.wyspr@gmail.com>
2026-01-10 11:23:15 -0500

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# BTA Hats
+
+Renders certain blocks as hats when worn on head.
+
---
+++ src/main/java/com/wyspr/hats/Hats.java
@@ -0,0 +1,15 @@
+package com.wyspr.hats;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Hats implements ClientModInitializer {
+	public static final String MOD_ID      = "Hats";
+	public static final Logger LOGGER      = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("Hats initialized");
+	}
+}
